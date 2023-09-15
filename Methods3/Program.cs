2634027739cf3588eeef9cef2dc3bdc3aca2dcd0
sport <sport@LAPTOP-61I2ACB0>
2023-09-15 22:58:55 +0300

    Add project files.

---
+++ Methods3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Enter a number greater than 0: ");
+int term = Convert.ToInt32(Console.ReadLine());
+
+    int result = Fibonacci(term);
+    Console.WriteLine("The Fibonacci number of term " + term + " is " + result);
+
+static int Fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
